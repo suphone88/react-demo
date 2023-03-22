90f614419072a04d4af189f39e4125234f19bc03
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-22 15:50:04 +0630

    update vercel react-demo

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.json("Hello World!!");
-});
-
-app.listen(5000, () => console.log("server is running..."));
